c688941a8468f03e39edc092b3faad57031bc3f2
Angelo Fuchs <af@steamnet.de>
2021-07-03 17:24:18 +0200

    forgot the ... main ... in the convention.

--- build.sh
+++ build.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
-CLASSPATH="src/java"
+CLASSPATH="src/main/java"
 
-javac -cp "$CLASSPATH" src/java/*.java
-javac -cp "$CLASSPATH" src/java/chat/delta/java/*.java
+javac -cp "$CLASSPATH" src/main/java/*.java
+javac -cp "$CLASSPATH" src/main/java/chat/delta/java/*.java
 
 
-cd src/cpp/chat/delta/native
+cd native_project/src/main/c/chat/delta/native
 
 gcc \
 dc_wrapper.c \
 -I /usr/lib/jvm/java-8-openjdk/include -I /usr/lib/jvm/java-8-openjdk/include/linux/ \
 -L libdeltachat.a -l:./libdeltachat.a \
--o libdeltajni.so -shared
+-o ../../../../../target/libdeltajni.so -shared
 
-cd ../../../../..
+cd ../../../../../../..
+pwd
 
 # package
 mkdir -p target || true
 rm target/* || true
 
-cd src/java/
+cd src/main/java/
 jar cf ../../target/deltachat.jar chat/delta/java/*.class
-cd ../../
-cp src/cpp/chat/delta/native/libdeltajni.so target/libdeltajni.so
+cd ../../..
+cp native_project/target/libdeltajni.so target/libdeltajni.so
